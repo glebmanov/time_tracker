85fc62a41afb61012f0126aee97d640d77a01a35
glebmanov <glebmanov@gmail.com>
2025-05-01 23:34:19 +0300

    remove app template

--- lib/time_tracker_web.ex
+++ lib/time_tracker_web.ex
@@ -53,7 +53,7 @@ defmodule TimeTrackerWeb do
   def live_view do
     quote do
       use Phoenix.LiveView,
-        layout: {TimeTrackerWeb.Layouts, :app}
+        layout: {TimeTrackerWeb.Layouts, :root}
 
       unquote(html_helpers())
     end
